6a9b9d7a838ee5083dd3f35b804c10416d21832b
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-26 03:09:47 +0600

    Selected Player UI Completed

--- src/components/Players/SelectedPlayers/SelectedPlayer/SelectedPlayer.jsx
+++ src/components/Players/SelectedPlayers/SelectedPlayer/SelectedPlayer.jsx
@@ -1,30 +1,36 @@
 import { RiDeleteBin5Line } from "react-icons/ri";
-import "../../../../index.css"
+import "../../../../index.css";
 
 const SelectedPlayer = ({ selectedPlayer }) => {
-    console.log(selectedPlayer)
-    return (
-      <div className="flex justify-between gap-4">
-        <div className="flex gap-4">
-          <figure className="overflow-hidden rounded-xl">
-            <img
-              className="w-16 h-16 object-cover rounded-xl"
-              src={selectedPlayer.image}
-              alt={selectedPlayer.name}
-            />
-          </figure>
-          <div>
-            <h1 className="text-2xl font-semibold text-black">
-              {selectedPlayer.name}{" "}
-            </h1>
-            <p className="text-gray-700">{selectedPlayer.battingStyle}</p>
+  console.log(selectedPlayer);
+  return (
+    <>
+      <div className=" border border-gray-50 shadow-sm rounded-2xl mb-4 bg-white">
+        <div className="flex justify-between items-center p-3">
+          <div className="flex items-center gap-3">
+            <figure className="overflow-hidden rounded-xl">
+              <img
+                className="w-14 h-14 object-cover rounded-xl"
+                src={selectedPlayer.image}
+                alt={selectedPlayer.name}
+              />
+            </figure>
+            <div>
+              <h1 className="text-lg font-semibold text-black">
+                {selectedPlayer.name}{" "}
+              </h1>
+              <p className="text-sm text-gray-500 mt-1">
+                {selectedPlayer.battingStyle}
+              </p>
+            </div>
           </div>
+          <button type="button" className="flex items-center justify-center">
+            <RiDeleteBin5Line className="text-red-500 text-lg cursor-pointer" />
+          </button>
         </div>
-        <button type="button" cursor-pointer>
-          <RiDeleteBin5Line />
-        </button>
       </div>
-    );
+    </>
+  );
 };
 
 export default SelectedPlayer;
--- src/components/Players/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/Players/SelectedPlayers/SelectedPlayers.jsx
@@ -3,13 +3,24 @@ import SelectedPlayer from "./SelectedPlayer/SelectedPlayer";
 
 const SelectedPlayers = ({ selectedPlayers }) => {
   
-    return <div>
+    return (
+      <div>
         <div>
-            {
-                selectedPlayers.map(selectedPlayer=><SelectedPlayer key={selectedPlayer.id} selectedPlayer={selectedPlayer}></SelectedPlayer>)
-            }
+          {selectedPlayers.map((selectedPlayer) => (
+            <SelectedPlayer
+              key={selectedPlayer.id}
+              selectedPlayer={selectedPlayer}
+            ></SelectedPlayer>
+          ))}
+
+          <div className="border border-lime-400 p-1 rounded-2xl inline-block">
+            <button className="btn bg-[#E7FE29] border-none text-black font-bold rounded-xl hover:bg-lime-300">
+              Add More Player
+            </button>
+          </div>
+        </div>
       </div>
-  </div>;
+    );
 };
 
 export default SelectedPlayers;
