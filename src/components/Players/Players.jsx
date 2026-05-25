a442b0f3f21c9de2e93d92eb6fefeae38cabb7bc
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-25 23:30:13 +0600

    Toggle Function Done

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,14 +1,36 @@
-import { use } from "react";
-import "../../index.css"
+import { use, useState } from "react";
+import "../../index.css";
 import AvailablePlayers from "./AvailablePlayers/AvailablePlayers";
 const Players = ({ playerPromise }) => {
     const players = use(playerPromise);
-    
-    return (
-        <div className="max-w-7xl mx-auto px-4">
-        <AvailablePlayers players={players}></AvailablePlayers>
+    const [available, setAvailable] = useState("available");
+
+  return (
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="flex justify-between items-center mb-6">
+        {available === "available" ? (
+          <h1 className="font-bold text-2xl">Available Players</h1>
+        ) : (
+          <h1 className="font-bold text-2xl">Selected Players</h1>
+        )}
+        <div>
+          <button
+            onClick={() => setAvailable("available")}
+            className={`btn ${available === "available" ? "btn-primary" : "btn"} rounded-r-none`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setAvailable("selected")}
+            className={`btn ${available === "selected" ? "btn-primary" : "btn"} rounded-l-none`}
+          >
+            Selected (0)
+          </button>
+        </div>
       </div>
-    );
+      <AvailablePlayers players={players}></AvailablePlayers>
+    </div>
+  );
 };
 
 export default Players;
